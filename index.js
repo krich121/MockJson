a37992fb240f2f864df63f77302b8ae6e5983fa3
Krich_Run <Krich_Run@freewillsolutions.com>
2021-09-17 15:52:40 +0700

    Fix

--- index.js
+++ index.js
@@ -123,7 +123,11 @@ app.get('/getmockdata', (req, res) => {
     }
 })
 
-app.listen(PORT, () => {
-    //console.log(`Server started on port ${PORT}`);
-    console.log('Server started');
-})
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+//app.listen(PORT, () => {
+//    //console.log(`Server started on port ${PORT}`);
+//    console.log('Server started');
+//})
